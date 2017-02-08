380d0f24aa84d702b70f31aacee9e37315956de1
FrancoNegri <franconegri2004@hotmail.com>
2017-02-08 02:07:56 -0300

    [add] cambiadas algunas cosas de la interfaz para mayor claridad en la respuesta

--- routes/response.js
+++ routes/response.js
@@ -6,23 +6,23 @@ var app = express();
 var sqlite3 = require('sqlite3').verbose()
 var db = new sqlite3.Database('database.db')
 
-var stmt = db.prepare('INSERT INTO results VALUES (?,?,?,?,?,?,?)')
+var stmt = db.prepare('INSERT INTO results VALUES (?,?,?,?,?,?,?,?)')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-
+  res.status(status).send("Hello world, this should be sent inmediately");
 	var ip = req.hostname;
 	var genero = unescape(req.query.genero);
 	var edad = unescape(req.query.edad);
 	var provincia = unescape(req.query.provincia);
 	var trans = unescape(req.query.trans);
 	var nac = unescape(req.query.nac);
-	var audio = unescape(req.query.audio);
-    console.log( "Upcoming response from: " + ip + " genero: " + genero + " provincia: " + provincia + " transcription: " + trans + " LANG: " + nac + " audio: " + audio );
-    //CREATE TABLE results(IP INT,EDAD INT,GENERO TEXT,PROVINCIA TEXT,LANG TEXT, TRANSC TEXT);
-    stmt.run(ip,audio, edad,genero,provincia,nac,trans );
-    res.send('respond with a resource');
+  var oracion = unescape(req.query.oracion);
+  var nivel = unescape(req.query.nivel);
+  console.log( "Upcoming response from: " + ip + " oracion: " + oracion + " nivel: " + nivel + " audio: "+ audio + " genero: " + genero + " provincia: " + provincia + " transcription: " + trans + " LANG: " + nac + " audio: " + audio );
+  //CREATE TABLE results(IP INT,ORACION INT, NIVEL INT,EDAD INT,GENERO TEXT,PROVINCIA TEXT,LANG TEXT, TRANSC TEXT);
+  stmt.run(ip,oracion,nivel, edad,genero,provincia,nac,trans );
 });
 
 
