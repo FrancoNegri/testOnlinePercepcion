fa2aab35e4efee446f7c47392ef296d07b2abc3d
Franco Negri <franconegri2004@gmail.com>
2017-10-04 21:28:33 -0300

    Added some more logic to the poll, added cookies so not reasking is necesary

--- routes/response.js
+++ routes/response.js
@@ -9,79 +9,81 @@ var db = new sqlite3.Database('database.db')
 var stmt = db.prepare('INSERT INTO results VALUES (?,?,?,?,?,?,?,?)')
 
 router.get('/', function(req, res, next) {
-  
-  var error = false;
-  if(req.query.oracion && req.query.nivel && req.query.edad){
-    var oracion = parseInt(unescape(req.query.oracion));
-    var nivel = parseInt(unescape(req.query.nivel));
-    var edad = parseInt(unescape(req.query.edad));
-    if(isNaN(oracion) || isNaN(nivel) || isNaN(edad)){
-      error = true;
-    }
-  }else{
-    error = true;
-  }
-  if(req.query.genero && req.query.provincia && req.query.nac && req.query.trans){
-    var ip = ipV4StringToInteger(req.hostname);
-    var genero = unescape(req.query.genero);
-    var provincia = unescape(req.query.provincia);
-    var nac = unescape(req.query.nac);
-    var trans = unescape(req.query.trans);
-  }else{
-    error = true;
-  }
-  if(error){
-    console.log("invalid data!");
-    res.send('invalid');
-    return;
-  }
-  console.log( "Upcoming response from: ")
-  console.log( req.hostname)
-  console.log(ip)
-  console.log(" oracion: ")
-  console.log(oracion)
-  console.log(" nivel: ")
-  console.log(nivel)
-  console.log(" genero: ")
-  console.log(genero)
-  console.log(" edad: ")
-  console.log(edad)
-  console.log(" provincia: ")
-  console.log(provincia)
-  console.log(" LANG: ")
-  console.log(nac)
-  console.log(" transcription: ")
-  console.log(trans)
 
-  //CREATE TABLE results(IP INT PRIMARY KEY,ORACION INT, NIVEL INT,EDAD INT,GENERO TEXT,PROVINCIA TEXT,LANG TEXT, TRANSC TEXT);
-  //IP como int? ver mejor esto...
-  // que pasa si alguien pone "====" en un formulario
-  stmt.run(ip,oracion,nivel, edad,genero,provincia,nac,trans );
-  res.send('ok');
+    var error = false;
+    if (req.query.oracion && req.query.nivel && req.query.edad) {
+        var oracion = parseInt(unescape(req.query.oracion));
+        var nivel = parseInt(unescape(req.query.nivel));
+        var edad = parseInt(unescape(req.query.edad));
+        if (isNaN(oracion) || isNaN(nivel) || isNaN(edad)) {
+            error = true;
+        }
+    } else {
+        error = true;
+    }
+    if (req.query.genero && req.query.provincia && req.query.nac && req.query.trans) {
+        var ip = ipV4StringToInteger(req.hostname);
+        var genero = unescape(req.query.genero);
+        var provincia = unescape(req.query.provincia);
+        var nac = unescape(req.query.nac);
+        var trans = unescape(req.query.trans);
+    } else {
+        error = true;
+    }
+    if (error) {
+        console.log("invalid data!");
+        res.send('invalid');
+        return;
+    }
+    console.log("Upcoming response from: ")
+    console.log(req.hostname)
+    console.log(ip)
+    console.log("oracion: ")
+    console.log(oracion)
+    console.log("nivel: ")
+    console.log(nivel)
+    console.log("genero: ")
+    console.log(genero)
+    console.log("edad: ")
+    console.log(edad)
+    console.log("provincia: ")
+    console.log(provincia)
+    console.log("LANG: ")
+    console.log(nac)
+    console.log("transcription: ")
+    console.log(trans)
+
+    //CREATE TABLE results(IP INT PRIMARY KEY,ORACION INT, NIVEL INT,EDAD INT,GENERO TEXT,PROVINCIA TEXT,LANG TEXT, TRANSC TEXT);
+    //IP como int? ver mejor esto...
+    // que pasa si alguien pone "====" en un formulario
+    stmt.run(ip, oracion, nivel, edad, genero, provincia, nac, trans);
+    res.send('ok');
 });
 
-
 var ipV4StringToInteger = function(string) {
-  var parts = string.split(".");
-  if(parts.length != 4)
-    throw new Error("IPv4 string does not have 4 parts.");
+    var hola;
 
-  var sum = 0;
+    console.log(string)
+    var parts = string.split(".");
+    if (parts.length != 4)
+        throw new Error("IPv4 string does not have 4 parts.");
 
-  for(var i = 0; i < 4; i++) {
+    var sum = 0;
 
-    var part = parts[i];
-    if(!part.match("^\\d+$"))
-      throw new Error("Non-digit, non-period character in IPv4 string.");
+    for (var i = 0; i < 4; i++) {
 
-    var partVal = Number(part);
-    if(partVal > 255)
-      throw new Error("IPv4 string contains invalid value.");
+        var part = parts[i];
+        if (!part.match("^\\d+$"))
+            throw new Error("Non-digit, non-period character in IPv4 string.");
 
-    sum = (sum << 8) + partVal;
-  }
+        var partVal = Number(part);
+        if (partVal > 255)
+            throw new Error("IPv4 string contains invalid value.");
 
-  return sum;
+        sum = (sum << 8) + partVal;
+    }
+
+    return sum;
 };
 
 module.exports = router;
--- views/scripts.js
+++ views/scripts.js
@@ -1,43 +1,59 @@
-  function submit(oracion, nivel){
+  function submit(oracion, nivel) {
+      //datos de la encuesta
       var nac = document.getElementById('nac').value
       var trans = document.getElementById('trans').value
-      var edad = document.getElementById('edad').value
-      var provincia = document.getElementById('provincia').value
-      var genero = document.getElementById('genero').value
+          //datos personales
+      var timesComplet = timesCompleted()
 
-      var timesCompleted = checkIfCompleted()
-
-      if(nac == ""){
-        var guardarAlert = document.getElementById('guardarAlert');
-        guardarAlert.className = "alert alert-danger"
-        return
+      if (nac == "") {
+          var guardarAlert = document.getElementById('guardarAlert');
+          guardarAlert.className = "alert alert-danger"
+          return
       }
 
+      var [edad, provincia, genero] = getPersonalData();
       var xmlHttp = new XMLHttpRequest();
-      xmlHttp.open( "GET", "response?nac=" + nac + "&trans=" + trans + "&genero=" + genero + "&provincia=" + provincia + "&edad=" + edad + "&oracion=" + oracion + "&nivel=" + nivel, true );
-      xmlHttp.send( null );
-      timesCompleted++
-      document.cookie = "submisionCompleted=" + timesCompleted  + ";";
-      if(timesCompleted < 5){
-        document.body.innerHTML="Gracias por participar, en instantes se le proporcionará otro audio para escuchar, si desea salir, puede hacerlo ahora y continuar mas adelante...";
-        var previousAudios = getCookie("audiosCompleted")
-        if(previousAudios == ""){
-            document.cookie = "audiosCompleted=" + oracion + ";";
-            window.location += "?audiosCompleted=" + oracion;  
-        }
-        else{
-            document.cookie = "audiosCompleted=" + previousAudios + "," + oracion + ";";
-            window.location += "?audiosCompleted=" + previousAudios + "," + oracion
-        }
-        setTimeout(function(){ location.reload(); }, 7000);
+      xmlHttp.open("GET", "response?nac=" + nac + "&trans=" + trans + "&genero=" + genero + "&provincia=" + provincia + "&edad=" + edad + "&oracion=" + oracion + "&nivel=" + nivel, true);
+      xmlHttp.send(null);
+      timesComplet++
+      document.cookie = "timesComplet=" + timesComplet + ";";
+      if (timesComplet < 5) {
+          document.body.innerHTML = "Gracias por participar, en instantes se le proporcionará otro audio para escuchar, si desea salir, puede hacerlo ahora y continuar mas adelante...";
+          var previousAudios = getCookie("audiosCompleted")
+          if (previousAudios == "") {
+              document.cookie = "audiosCompleted=" + oracion + ";";
+              window.location += "?audiosCompleted=" + oracion;
+          } else {
+              document.cookie = "audiosCompleted=" + previousAudios + "," + oracion + ";";
+              window.location += "," + oracion;
+          }    
+      setTimeout(reloadPage, 8000)
+      } else {
+          document.body.innerHTML = "Gracias por participar, eso es todo, si querés parcipar de más experimentos podés entrar en...";
       }
-      else
-        document.body.innerHTML="Gracias por participar, eso es todo, si querés parcipar de más experimentos podés entrar en...";
   }
+
+  function reloadPage() {
+      location.reload()
+  }
+
+  function getPersonalData() {
+      if (getCookie("personalInfo") === "") {
+          var edad = document.getElementById('edad').value
+          var provincia = document.getElementById('provincia').value
+          var genero = document.getElementById('genero').value
+      } else {
+          var edad = getCookie("edad");
+          var provincia = getCookie("provincia");
+          var genero = getCookie("genero");
+      }
+      return [edad, provincia, genero];
+  }
+
   function getCookie(cname) {
       var name = cname + "=";
       var ca = document.cookie.split(';');
-      for(var i = 0; i < ca.length; i++) {
+      for (var i = 0; i < ca.length; i++) {
           var c = ca[i];
           while (c.charAt(0) == ' ') {
               c = c.substring(1);
@@ -49,71 +65,94 @@
       return "";
   }
 
-function checkIfCompleted(){
-  var sub = getCookie("submisionCompleted");
-  if (sub == "") {
-    var all = document.getElementById("all");
-      all.className = "";
-      return 0
-    }
+  function checkIfCompleted() {
+      if (timesCompleted() > 4) {
+          document.body.innerHTML = "Gracias por participar, eso es todo, si querés parcipar de más experimentos podés entrar en...";
+      } else {
+          var personalInfo = getCookie("personalInfo")
+          if (personalInfo === "") {
+              showPoll();
+          } else {
+              showInstructions();
+              showPoll();
+          }
+      }
+  }
 
-  sub = parseInt(sub)
-  if (sub < 5) {
+  function showPoll() {
       var all = document.getElementById("all");
       all.className = "";
-      return sub
-  }else{
-    document.body.innerHTML="Gracias por participar, eso es todo, si querés parcipar de más experimentos podés entrar en...";
-    return 5
   }
-}
 
-function guardar(){
-  var edad = document.getElementById('edad').value;
-  var provincia = document.getElementById('provincia').value;
-  var genero = document.getElementById('genero').value;
-  var missing = false;
-  if(edad == -1){
-      missing = true;
-      var edadAlert = document.getElementById('edadAlert');
-      edadAlert.className = "alert alert-danger"
-  }
-  if(genero == "-"){
-      missing = true;
-      var generoAlert = document.getElementById('generoAlert');
-      generoAlert.className = "alert alert-danger"
-  }
-  if(provincia == "-"){
-      missing = true;
-      var provinciaAlert = document.getElementById('provinciaAlert');
-      provinciaAlert.className = "alert alert-danger"
-  }
-  if(missing)
-      return;
-  var datosPersonales = document.getElementById("datosPersonales");
-  datosPersonales.className = "hide";
-  var infoGral = document.getElementById("infoGral");
-  infoGral.className = "hide";
-  var instrucciones = document.getElementById("instrucciones");
-  instrucciones.className = "btn-group.bootstrap-select"
 
-}
-var reproduccionesRestantes = 2;
-var notified = false;
-function play(){
-  var oAudio = document.getElementById('myaudio');
-  if(!oAudio.paused)
-    return;
-  reproduccionesRestantes--;
-  document.getElementById("contador").innerHTML = "Te quedan " + reproduccionesRestantes + " reproducciones";
-  oAudio.play();
-  if(reproduccionesRestantes == 0)
-    document.getElementById('playExp').disabled="disabled"
-}
+  function timesCompleted() {
+      var sub = getCookie("timesComplet");
+      if (sub === "") {
+          return 0
+      }
+      sub = parseInt(sub)
+      if (sub < 5) {
+          return sub
+      } else {
+          return 5
+      }
+  }
 
-function entendido(){
-  var instrucciones = document.getElementById("instrucciones");
-  instrucciones.className = "hide"
-  var experimento = document.getElementById("experimento");
-  experimento.className = "form-group";
-}
+  function guardar() {
+      var edad = document.getElementById('edad').value;
+      var provincia = document.getElementById('provincia').value;
+      var genero = document.getElementById('genero').value;
+      var missing = false;
+      if (edad == -1) {
+          missing = true;
+          var edadAlert = document.getElementById('edadAlert');
+          edadAlert.className = "alert alert-danger"
+      }
+      if (genero == "-") {
+          missing = true;
+          var generoAlert = document.getElementById('generoAlert');
+          generoAlert.className = "alert alert-danger"
+      }
+      if (provincia == "-") {
+          missing = true;
+          var provinciaAlert = document.getElementById('provinciaAlert');
+          provinciaAlert.className = "alert alert-danger"
+      }
+      if (missing)
+          return;
+      document.cookie = "personalInfo=ok;";
+      document.cookie = "edad=" + edad + ";";
+      document.cookie = "provincia=" + provincia + ";";
+      document.cookie = "genero=" + genero + ";";
+      showInstructions();
+  }
+
+  function showInstructions() {
+      var datosPersonales = document.getElementById("datosPersonales");
+      datosPersonales.className = "hide";
+      var infoGral = document.getElementById("infoGral");
+      infoGral.className = "hide";
+      var instrucciones = document.getElementById("instrucciones");
+      instrucciones.className = "btn-group.bootstrap-select"
+  }
+
+  var reproduccionesRestantes = 2;
+  var notified = false;
+
+  function play() {
+      var oAudio = document.getElementById('myaudio');
+      if (!oAudio.paused)
+          return;
+      reproduccionesRestantes--;
+      document.getElementById("contador").innerHTML = "Te quedan " + reproduccionesRestantes + " reproducciones";
+      oAudio.play();
+      if (reproduccionesRestantes == 0)
+          document.getElementById('playExp').disabled = "disabled"
+  }
+
+  function entendido() {
+      var instrucciones = document.getElementById("instrucciones");
+      instrucciones.className = "hide"
+      var experimento = document.getElementById("experimento");
+      experimento.className = "form-group";
+  }
