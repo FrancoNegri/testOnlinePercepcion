2cbd8d7c2509cdd951ded7361757a56b5289a1aa
Franco Negri <franconegri2004@gmail.com>
2017-10-04 22:01:55 -0300

    Change for better ip handling

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var index = require('./routes/index').init123();
 var response = require('./routes/response');
 var helmet = require('helmet');
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -26,6 +25,7 @@ app.use(helmet())
 app.use('/', index);
 app.use('/response', response);
 
+app.set('trust proxy', true)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
